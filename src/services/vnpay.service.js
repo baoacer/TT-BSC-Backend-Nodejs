0ab8ddbfa9faf29e53390e24a2ef98c4c343c4aa
baoacer <nguyenquocbaotu@gmail.com>
2025-05-28 13:03:00 +0700

    FIX: vnpay

--- src/services/vnpay.service.js
+++ src/services/vnpay.service.js
@@ -1,5 +1,4 @@
 "use strict";
-require("dotenv").config();
 const querystring = require("qs");
 const crypto = require("crypto");
 const moment = require("moment");
@@ -17,16 +16,16 @@ const createPaymentUrl = async ({
   // let orderId = moment(date).format('DDHHmmss');
   
   let vnp_Params = {
-    vnp_Version: process.env.VNPAY_VERSION || "2.1.0",
+    vnp_Version: "2.1.0",
     vnp_Command: "pay", // thanh toán
-    vnp_TmnCode: process.env.VNPAY_TMN_CODE, // website code
+    vnp_TmnCode: 'BV4AURGO', // website code
     vnp_Locale: "vn", // language
-    vnp_CurrCode: process.env.VNPAY_CURRENCY, // currency
+    vnp_CurrCode: 'VND', // currency
     vnp_TxnRef: orderId, // mã đơn hàng
     vnp_OrderInfo: "Thanh toan don hang",
     vnp_OrderType: orderType || "others", // loại hàng hóa
     vnp_Amount: amount,
-    vnp_ReturnUrl: process.env.VNPAY_RETURN_URL,
+    vnp_ReturnUrl: 'http://nguyenlequocbao.id.vn/v1/api/vnpay/handle_payment_response',
     vnp_IpAddr: ipAddr,
     vnp_CreateDate: createDate,
   };
@@ -34,10 +33,10 @@ const createPaymentUrl = async ({
   vnp_Params = Utils.sortObject(vnp_Params); // sắp xếp thứ tự tham số A-Z
 
   const signData = querystring.stringify(vnp_Params, { encode: false });
-  const hmac = crypto.createHmac("sha512", process.env.VNPAY_HASH_SECRET);
+  const hmac = crypto.createHmac("sha512", 'NYPDINBB0ZIAB2MBA17N7DSM6JYD1OJO');
   const signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
   vnp_Params.vnp_SecureHash = signed;
-  const paymentUrl = `${process.env.VNPAY_URL}?${querystring.stringify(
+  const paymentUrl = `https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?${querystring.stringify(
     vnp_Params,
     { encode: false },
   )}`;
@@ -52,7 +51,7 @@ const handlePaymentResponse = (vnp_Params = {}) => {
   vnp_Params = Utils.sortObject(vnp_Params);
   const signData = querystring.stringify(vnp_Params, { encode: false });
   const signed = crypto
-    .createHmac("sha512", process.env.VNPAY_HASH_SECRET)
+    .createHmac("sha512", 'NYPDINBB0ZIAB2MBA17N7DSM6JYD1OJO')
     .update(Buffer.from(signData, "utf-8"))
     .digest("hex");
 
