ac2315fe51f37fb7f3728484398b5e4907504822
baoacer <nguyenquocbaotu@gmail.com>
2025-06-21 23:57:10 +0700

    UP

--- src/routers/template/index.js
+++ src/routers/template/index.js
@@ -4,7 +4,7 @@ const TemplateController = require('../../controllers/template.controller')
 const router = express.Router()
 const {authenticateJWT, requireRole} = require('../../middleware/auth.middleware')
 const { ROLE } = require('../../configs/contants')
-
-router.post('', requireRole(ROLE.ADMIN), asyncHandler(TemplateController.createTemplate))
+// requireRole(ROLE.ADMIN),
+router.post('', asyncHandler(TemplateController.createTemplate))
 
 module.exports = router
