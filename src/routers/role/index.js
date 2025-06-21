e7121a01026697ddc5bf34c2ca78c7a9bf35235f
baoacer <nguyenquocbaotu@gmail.com>
2025-06-21 23:54:55 +0700

    UPDATE

--- src/routers/role/index.js
+++ src/routers/role/index.js
@@ -4,7 +4,7 @@ const RoleController = require('../../controllers/role.controller');
 const { requireRole } = require('../../middleware/auth.middleware');
 const router = express.Router();
 
-router.use(requireRole('Admin'));
+// router.use(requireRole('Admin'));
 
 router.post('/', asyncHandler(RoleController.createRole));
 router.post('/assign', asyncHandler(RoleController.assignRoleToUser));
