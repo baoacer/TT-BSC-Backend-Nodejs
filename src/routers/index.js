49a20832d53b67197e7c7b9186c52def4a94caa2
baoacer <nguyenquocbaotu@gmail.com>
2025-06-22 12:55:16 +0700

    UP

--- src/routers/index.js
+++ src/routers/index.js
@@ -15,11 +15,11 @@ router.use('/v1/api/category', require('./category/index'))
 router.use('/v1/api/chat', require('./chat/index'))
 router.use('/v1/api/product', require('./product/index'))
 router.use('/v1/api/vnpay', require('./vnpay/index'))
+router.use('/v1/api/role', require('./role/index'))
 
 router.use(authenticateJWT)
 
 router.use('/v1/api/order', require('./order/index'))
-router.use('/v1/api/role', require('./role/index'))
 router.use('/v1/api/cart', require('./cart/index'))
 router.use('/v1/api/template', require('./template/index'))
 
